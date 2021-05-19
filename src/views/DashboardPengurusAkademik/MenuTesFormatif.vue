fc5a7eb54308255a125aca7d1c6b76c97b2378e9
Ardiafrizky <ardiafrizky24@gmail.com>
2021-05-19 11:23:39 +0700

    multiple NTF debug

--- src/views/DashboardPengurusAkademik/MenuTesFormatif.vue
+++ src/views/DashboardPengurusAkademik/MenuTesFormatif.vue
@@ -3,15 +3,15 @@
     title= "Dashboard Tes Formatif"
     icon= "edit">
   </MainHeader>
-    <div class="container">
+    <div class="container mt-n10">
         <div class="col-m-4 col-xl-6 mb-4 mt-4" style="margin:auto;">
         <div class="card card-header-actions">
             <div class="card-header">
                 Upload File Tes Formatif
-                <button id="submitButton" class="btn btn-success btn-sm" v-on:click="submitFile()">Simpan File</button>
+                <button id="submitButton" class="btn btn-success btn-sm" data-toggle="modal" data-target="#saveModal" v-on:click="submitFile()">Simpan File</button>
             </div>
             <div class="card-body">
-                <p>*file .xls (max: 1MB)</p>
+                <p>*file .xls/.xlsx (max: 1MB), dengan nama file tanggal ujian (e.g: 2020-09-17.xls).</p>
                 <form class="container container-file" style="padding:0;">
                     <span class="btn btn-primary btn-file">
                         Pilih File (.xls) <input id="fileInput" type="file" name="file" ref="file" v-on:change="handleFileUpload()">
@@ -27,53 +27,106 @@
     </div>
     
     <div class="col-xxl-12 col-xl-12 mb-4 mt-4">
-      <div class="card">
-        <div class="card-header">Nilai Tes Formatif</div>
-        <div class="card-body">
-          <div class="tab-content" id="cardTabContent">
-                <!-- ON GOING -->
-            <div
-                class="tab-pane fade show active"
-                id="ongoing"
-                role="tabpanel"
-                aria-labelledby="ongoing-tab"
-            >
-            <div class="datatable" v-if="isDataTableReady">
-                <table
-                    class="table table-bordered table-hover"
-                    id="dataTable"
-                    width="100%"
-                    cellspacing="0"
-                >
-                    <thead>
-                    <tr>
-                        <th>Tanggal Input</th>
-                        <th>Tanggal Ujian</th>
-                        <th>Nama</th>
-                        <th>NPM</th>
-                        <th>Nilai</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr
-                        v-for="item in tesformatif"
-                        v-bind:key="item.idNilaiTesFormatif"
+        <div class="card">
+            <div class="card-header">Nilai Tes Formatif</div>
+            <div class="card-body">
+                <div class="tab-content" id="cardTabContent">
+                    <!-- ON GOING -->
+                    <div
+                        class="tab-pane fade show active"
+                        id="ongoing"
+                        role="tabpanel"
+                        aria-labelledby="ongoing-tab"
                     >
-                        <td>{{ item.tanggalUnggah }}</td>
-                        <td>{{ item.tanggalTes }}</td>
-                        <td>{{ item.nama }}</td>
-                        <td>{{ item.npm }}</td>
-                        <td>{{ item.nilai }}</td>
-                    </tr>
-                    </tbody>
-                </table>
+                        <div class="datatable" v-if="isDataTableReady">
+                            <table
+                                class="table table-bordered table-hover"
+                                id="dataTable"
+                                width="100%"
+                                cellspacing="0"
+                            >
+                                <thead>
+                                <tr>
+                                    <th>Tanggal Input</th>
+                                    <th>Tanggal Ujian</th>
+                                    <th>Nama</th>
+                                    <th>NPM</th>
+                                    <th>Nilai</th>
+                                    <th>Hapus Data</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr
+                                    v-for="item in tesformatif"
+                                    v-bind:key="item.idNilaiTesFormatif"
+                                >
+                                    <td>{{ item.tanggalUnggah }}</td>
+                                    <td>{{ item.tanggalTes }}</td>
+                                    <td>{{ item.nama }}</td>
+                                    <td>{{ item.npm }}</td>
+                                    <td>{{ item.nilai }}</td>
+                                    <td><button @click="sendInfo(item)" class="btn btn-danger mr-2" data-toggle="modal" data-target="#deleteConfirmation">Hapus</button></td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
             </div>
-        </div>
-        </div>
         </div>
     </div>
-  </div>
+
+      <!-- Save Confirmation Modal -->
+    <div class="modal fade" id="saveModal" tabindex="-1" role="dialog" aria-labelledby="saveModalTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="saveModalTitle">Pesan Pengunggahan File</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+                </div>
+                <span v-show="loading" class="spinner-border spinner-border-sm"></span>
+                <div v-if="successful" class="modal-body success-body">
+                    <i class="far fa-check-circle check-success"></i>
+                    File tes formatif berhasil ditambahkan ke dalam database.
+                </div>
+                <div v-if="!successful" class="modal-body fail-body">
+                    <i class="far fa-times-circle check-fail"></i>
+                    File tes formatif gagal ditambahkan ke database. Periksa kembali ekstensi, format, ukuran, dan nama file.
+                </div>
+                <!-- <div v-if="!successful && message" class="alert alert-danger mx-3" role="alert">{{message}}</div> -->
+                <div class="modal-footer">
+                    <button v-if="successful" @click="refreshPage" class="btn btn-light" type="button" data-dismiss="modal">OK</button>
+                    <button v-if="!successful" class="btn btn-light" type="button" data-dismiss="modal">Tutup</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Delete Confirmation Modal -->
+    <div class="modal fade" id="deleteConfirmation" tabindex="-1" role="dialog" aria-labelledby="deleteConfirmationTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="deleteConfirmationTitle">Penghapusan Data Tes</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+                </div>
+                <div v-if="isDeleteFail" class="modal-body fail-body">
+                    <i class="fas fa-times-circle check-fail"></i>
+                    {{ message }}
+                </div>
+                <div v-if="!deleted && !isDeleteFail" class="modal-body">Apakah anda yakin? Data ini ({{selectedItem.nama}} | {{selectedItem.tanggalTes}}) akan terhapus secara permanen dan tidak dapat dikembalikan lagi.</div>
+                <div v-if="deleted && !isDeleteFail" class="modal-body success-body">
+                    <i class="far fa-check-circle check-success"></i>
+                    Data tes berhasil dihapus.
+                </div>
+                <div class="modal-footer">
+                    <button v-if="!deleted && !isDeleteFail" @click="deleteNTF(selectedItem.idNilaiTesFormatif)" class="btn btn-danger" type="button">Hapus</button>
+                    <button @click="refreshPage" class="btn btn-light" type="button" data-dismiss="modal">Tutup</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -82,6 +135,7 @@ import MainHeader from "@/components/MainHeader.vue";
 import AreaChart from "@/components/AreaChartNTF.vue";
 import dataTableLoader from "@/js/datatable";
 import authHeader from "@/services/auth-header";
+import loadScript from "@/js/scripts.js";
 
 export default {
   name: "MenuResiden",
@@ -89,12 +143,17 @@ export default {
     return {
         file: "",
         message: "",
+        successful: null,
         tesformatif: null,
         isMounted: false,
         ready: false,
         chartReady: false,
         labels: null,
-        datas: null
+        datas: null,
+        deleted: false,
+        isDeleteFail: false,
+        isLoading: false,
+        selectedItem: {nama: "dummy", tanggalTes: "dummy"}
     };
   },
   components: {
@@ -131,48 +190,75 @@ export default {
     axios
         .get("http://localhost:8000/api/tes-formatif/summary", { headers: authHeader() })
         .then(success => {
-                this.labels = Object.keys(success.data).map((label) => label.substring(0,10));
+                this.labels = Object.keys(success.data).map((label) => new Date(label).toUTCString().substring(0,16));
                 this.datas = Object.values(success.data);
                 this.chartReady = true;
             },
             error => {
                 this.message =
                     (error.response && error.response.data && error.response.data.message) ||
-                    error.message ||
-                error.toString();
+                    error.message || error.toString();
             }
         );
+    loadScript();
     },
 
-  methods: {
-    handleFileUpload(){
-        this.file = this.$refs.file.files[0];
-        console.log(this.file)
-    },
+    methods: {
+        sendInfo(item) {
+            this.selectedItem = item;
+        },
 
-    submitFile(){
-        let formData = new FormData();
-        formData.append("file", this.file);
-        let header = authHeader()
-        header['Content-Type'] = 'multipart/form-data'
+        refreshPage(){
+            window.location.reload();
+        },
 
-        axios
-            .post('http://localhost:8000/api/tes-formatif/upload', formData, { headers: header})
-            .then(success => {
-                    this.successful = true;
-                    this.message = success.message || success.response || success.toString();
+        handleFileUpload(){
+            this.file = this.$refs.file.files[0];
+            console.log(this.file)
+        },
 
-                    // TODO: ALERT SUCCESS/FAIL
+        submitFile(){
+            let formData = new FormData();
+            formData.append("file", this.file);
+            let header = authHeader()
+            header['Content-Type'] = 'multipart/form-data'
+            this.isLoading = true
 
+            axios
+                .post('http://localhost:8000/api/tes-formatif/upload', formData, { headers: header})
+                .then(success => {
+                        this.successful = true;
+                        this.message = success.message || success.response || success.toString();
+                        this.isLoading = false;
+                        this.successful = true;
+                    },
+                    error => {
+                        this.message =                     
+                            (error.response && error.response.data && error.response.data.message) ||
+                            error.message || error.toString();
+                        this.successful = false;
+                        this.isLoading = false;
+                    }
+            )
+        },
+        deleteNTF(id) {
+            this.isLoading = true;
+            axios.delete('http://localhost:8000/api/tes-formatif/' + id, {headers: authHeader()}).then(
+                success => {
+                    this.deleted = true;
+                    this.isLoading = false;
                 },
                 error => {
-                    this.message = "Error pada Server, cek kembali file yang akan diunggah."
-                    this.successful = false
-                    console.log(error);
+                    this.message = "Data ini tidak dapat dihapus karena masih terhubung dengan entitas lain."
+                        // (error.response && error.response.data && error.response.data.message) ||
+                        // error.message ||
+                        // error.toString();
+                    this.isDeleteFail = true;
+                    this.isLoading = false;
                 }
-        )
+            );
+        }
     }
-  }
 };
 </script>
 
@@ -207,5 +293,20 @@ export default {
 .container-file {
     display: flex;
     flex-direction: column;
+}
+.success-body, .fail-body{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.check-success {
+    margin: 2rem;
+    font-size: 3rem;
+    color: rgba(54, 138, 54, 0.781);
+}
+.check-fail {
+    margin: 2rem;
+    font-size: 3rem;
+    color: rgb(202, 39, 39);
 }
 </style>
