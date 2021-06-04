ac7d4fff2a4e64b466e0c7797a3385325b099daa
Ardiafrizky <ardiafrizky24@gmail.com>
2021-06-04 22:29:45 +0700

    home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,18 @@
 <template>
   <MainHeader icon="home" :title="title" subtitle="Neulog - Departemen Neurologi FKUI - RSCM"/>
+    <div class="number-card-container container mt-n10">
+        <div class="number-card card">
+            <div class="card-body text-center">
+                <p class="name">
+                  {{ person.name }}
+                </p>
+                <p class="email">
+                  {{ person.email }}
+                </p>
+                <button @click="redirectToResidenRegister" class="btn btn-teal">Buat Akun Residen</button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -12,6 +25,12 @@ export default {
  computed: {
      title() {
          return "Selamat Datang, "+(this.$store.state.auth.user.username);
+     },
+     isResiden() {
+       return JSON.parse(localStorage.getItem('userData')).residen == null? false : true;
+     },
+     person() {
+       return JSON.parse(localStorage.getItem('userData'));
      }
  },
  created() {
